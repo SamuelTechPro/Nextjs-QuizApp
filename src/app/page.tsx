3c93f43ad5a9e3d9b24ebbfeb8439c459476deaa
f.samuel <samuelf8908@gmail.com>
2024-05-24 11:11:23 -0700

    fresh

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+
+
+import Link from "next/link"
+
+
+export default function Home(){
+    return(
+       <main>
+        <div className="container">
+            <h1>Quiz app</h1>
+            <Link href='/quiz'>
+                <button>Start Quiz</button>
+            </Link>
+        </div>
+
+       </main>
+        
+    )
+}
